9312dd727419d2e793668f4cb6f5b4ee12c91038
Ben Christel <bchristel@pivotal.io>
2021-10-31 16:14:35 -0700

    Transmit and merge the entire chatlog instead of just the last message

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,41 @@ import {h, Fragment} from "preact"
 import {useState, useEffect} from "preact/hooks"
 import {join} from "./chatroom"
 import type {Room, ConnectionStatus} from "./chatroom"
-import {append, toArray} from "./CrdtLog"
+import {append, toArray, merge} from "./CrdtLog"
 import type {CrdtLog} from "./CrdtLog"
+
+type ChatLog = CrdtLog<string>
 
 const myAgentId = String(Math.random())
 export function App(): JSX.Element {
-  const [room, setRoom] = useState<null | Room<string>>(null)
+  const [room, setRoom] = useState<null | Room<ChatLog>>(null)
   const [connStatus, setConnStatus] = useState<ConnectionStatus>("pending")
-  const [chatLog, setChatLog] = useState<CrdtLog<string>>({nextIndex: 0, entries: {}})
+  const [chatLog, setChatLog] = useState<ChatLog>({nextIndex: 0, entries: {}})
+
+  function getChatLog(): ChatLog {
+    let chatLog: ChatLog
+    setChatLog(x => chatLog = x)
+    return chatLog!
+  }
 
   useEffect(() => {
     join("ben", {
       handleConnectionStatusChanged: setConnStatus,
-      handleMessage(message) {
-        setChatLog(chatLog => append("", message, chatLog))
+      handleMessage(incoming: ChatLog) {
+        setChatLog(chatLog => merge(chatLog, incoming))
       },
-      getGreeting: () => "hello"
+      getGreeting: getChatLog,
     }).then(room => {
       setRoom(room)
     })
-  }, [])
+  }, []) // passing [] as the second argument prevents re-joining on every render
 
   return <ChatView
     connStatus={connStatus}
     onSubmit={message => {
-      room?.say(message)
-      setChatLog(append(myAgentId, message, chatLog))
+      const newChatLog = append(myAgentId, message, chatLog)
+      setChatLog(newChatLog)
+      room?.say(newChatLog)
     }}
     chatLog={chatLog}
   />
@@ -35,7 +44,7 @@ export function App(): JSX.Element {
 
 function ChatView(props: {
   connStatus: ConnectionStatus,
-  chatLog: CrdtLog<string>,
+  chatLog: ChatLog,
   onSubmit: (message: string) => unknown,
 }): JSX.Element {
   switch (props.connStatus) {
@@ -52,7 +61,7 @@ function ChatView(props: {
   }
 }
 
-function ChatLog(props: {data: CrdtLog<string>}): JSX.Element {
+function ChatLog(props: {data: ChatLog}): JSX.Element {
   return <ul>
     {toArray(props.data).map(message => <li>{message}</li>)}
   </ul>
